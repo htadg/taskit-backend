bd4be8f40a6f56db6628efeef4dfbe8dcffd2c44
Hiten Sharma <sharma.hiten3@gmail.com>
2024-07-25 22:51:54 +0530

    Maven setup

---
+++ src/main/java/com/htadg/taskit/TaskitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.htadg.taskit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskitBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/htadg/taskit/TaskitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htadg.taskit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
